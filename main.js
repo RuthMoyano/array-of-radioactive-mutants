ac6ee3319e5567fd005a8051d8332bfa64ea610b
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-03-15 09:48:39 -0500

    [add changes]

--- main.js
+++ main.js
@@ -2,9 +2,38 @@
  * YOUR CODE HERE *
  ******************/
 
+function addMeToEnd (arr) {
+arr.push('Colin');
+}
 
+function addMeToStart(arr) {
+  arr.unshift('Colin');
+}
 
+function changeLast(arr, value) {
+  arr[arr.length - 1] = value;
+}
+function changeAllValuesTo(arr, value) {
+  for (let i = 0; i < arr.length; i ++) {
+    arr[i] = value;
+  }
+}
 
+function oddOrEven(arr) {
+  for (let i = 0; i < arr.length; i ++) {
+    if (arr[i] % 2 === 1) {
+      arr[i] = 'odd';
+    } else {
+      arr[i] = 'even';
+    }
+  } 
+}
+
+function changeNextThreeToValue(index, arr, value) {
+  for (let i = index; i < index + 3; i ++) {
+    arr[i] = value;
+  }
+}
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
